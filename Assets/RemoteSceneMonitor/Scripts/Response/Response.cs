d7b999d5226aaa2e993fa262e0f864c2e065d71d
shda <arheuser@gmail.com>
2020-09-06 10:58:32 +0300

    Moving scripts to Script folder.

--- Assets/RemoteSceneMonitor/Scripts/Response/Response.cs
+++ Assets/RemoteSceneMonitor/Scripts/Response/Response.cs
@@ -6,12 +6,10 @@ namespace RemoteSceneMonitor
 {
     public abstract class Response
     {
-      //  protected SceneHierarchyData sceneHierarchyData;
         protected HttpServerContext context;
 
         public void SetInfo(HttpServerContext context)
         {
-        //    this.sceneHierarchyData = sceneHierarchyData;
             this.context = context;
         }
 
