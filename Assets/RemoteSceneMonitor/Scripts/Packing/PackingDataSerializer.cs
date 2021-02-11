06d6eeeee256a916fe0f1f04c7216b27ca84d222
shda <arheuser@gmail.com>
2021-02-11 22:22:55 +0300

    Fixed serialize to android.

--- Assets/RemoteSceneMonitor/Scripts/Packing/PackingDataSerializer.cs
+++ Assets/RemoteSceneMonitor/Scripts/Packing/PackingDataSerializer.cs
@@ -9,30 +9,48 @@ namespace RemoteSceneMonitor
     {
         public static string SerializeToString(PackingData packingData)
         {
-            using MemoryStream input = new MemoryStream();
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            binaryFormatter.Serialize(input, packingData);
-            input.Seek(0, SeekOrigin.Begin);
+            string outString = "";
 
-            using MemoryStream output = new MemoryStream();
-            using DeflateStream deflateStream = new DeflateStream(output, CompressionMode.Compress);
-            input.CopyTo(deflateStream);
-            deflateStream.Close();
-            return Convert.ToBase64String(output.ToArray());
+            using (var input = new MemoryStream())
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(input, packingData);
+                input.Seek(0, SeekOrigin.Begin);
+
+                using (var output = new MemoryStream())
+                {
+                    using (var deflateStream = new DeflateStream(output, CompressionMode.Compress))
+                    {
+                        input.CopyTo(deflateStream);
+                        deflateStream.Close();
+                    }
+
+                    outString = Convert.ToBase64String(output.ToArray());
+                }
+            }
+
+            return outString;
         }
-        
+
         public static PackingData DeserializeFromString(string text)
         {
-            using MemoryStream input = new MemoryStream(Convert.FromBase64String(text));
-            using DeflateStream deflateStream = new DeflateStream(input, CompressionMode.Decompress);
-            using MemoryStream output = new MemoryStream();
-            
-            deflateStream.CopyTo(output);
-            deflateStream.Close();
-            output.Seek(0, SeekOrigin.Begin);
+            PackingData message = null;
+            using (var input = new MemoryStream(Convert.FromBase64String(text)))
+            {
+                using (var deflateStream = new DeflateStream(input, CompressionMode.Decompress))
+                {
+                    using (var output = new MemoryStream())
+                    {
+                        deflateStream.CopyTo(output);
+                        deflateStream.Close();
+                        output.Seek(0, SeekOrigin.Begin);
 
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            PackingData message = (PackingData)binaryFormatter.Deserialize(output);
+                        BinaryFormatter binaryFormatter = new BinaryFormatter();
+                        message = (PackingData) binaryFormatter.Deserialize(output);
+                    }
+                }
+            }
+
             return message;
         }
     }
